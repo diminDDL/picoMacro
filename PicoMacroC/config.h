67e717ebaff7346549cda6ed7a931c702cf7b370
diminDDL <dimin21590@gmail.com>
2022-05-18 14:37:44 +0200

    ws2812 via PIO working

---
+++ PicoMacroC/config.h
@@ -0,0 +1,7 @@
+#define KEYBOARD_SIZE_X 4
+#define KEYBOARD_SIZE_Y 4
+#define WS2812_PIN 1
+#define WS2812_WHITE false
+#define WS2812_frequency 800000 // 800kHz
+
+#define NUM_OF_LEDS KEYBOARD_SIZE_X*KEYBOARD_SIZE_Y
--- PicoMacroC/main.c
+++ PicoMacroC/main.c
@@ -1,24 +1,50 @@
-#include <stdio.h>
 #include "pico/stdlib.h"
+#include "pico/bootrom.h"
+#include "hardware/pio.h"
+#include "hardware/clocks.h"
+#include "ws2812.pio.h"
+#include "config.h"
+
+static inline void put_pixel(uint32_t pixel_grb) {
+    pio_sm_put_blocking(pio0, 0, pixel_grb << 8u);
+}
+
+static inline uint32_t rgb_u32(uint8_t r, uint8_t g, uint8_t b) {
+    return ((uint32_t) (r) << 8) | ((uint32_t) (g) << 16) | (uint32_t) (b);
+}
+
+
 
 int main() {
-    const uint led_pin = 25;
-
-    // Initialize LED pin
-    gpio_init(led_pin);
-    gpio_set_dir(led_pin, GPIO_OUT);
-
-    // Initialize chosen serial port
     stdio_init_all();
+    static const uint led_pin = 25;
+    static const float pio_freq = 2000;
 
-    // Loop forever
+    // Choose PIO instance (0 or 1)
+    PIO pio = pio0;
+
+    // Get first free state machine in PIO 0
+    uint sm = pio_claim_unused_sm(pio, true);
+
+    // Add PIO program to PIO instruction memory. SDK will find location and
+    // return with the memory offset of the program.
+    uint offset = pio_add_program(pio, &WS_LED_program);
+
+    // Calculate the PIO clock divider
+    float div = (float)clock_get_hz(clk_sys) / pio_freq;
+
+    // Initialize the program using the helper function in our .pio file
+    ws_program_init(pio, sm, offset, WS2812_PIN, WS2812_frequency, WS2812_WHITE);
+
+    // Do nothing
+    int t = 0;
     while (true) {
-
-        // Blink LED
-        printf("Blinking!\r\n");
-        gpio_put(led_pin, true);
-        sleep_ms(1000);
-        gpio_put(led_pin, false);
-        sleep_ms(1000);
+        for(int8_t i = 0; i < NUM_OF_LEDS/3; i++) {
+            put_pixel(rgb_u32(255, 0, 0));
+            put_pixel(rgb_u32(0, 255, 0));
+            put_pixel(rgb_u32(0, 0, 255));
+        }
+        sleep_ms(100);
+        // reset_usb_boot(0,0); // allows us to reboot the pico in to the bootloader mode
     }
 }
